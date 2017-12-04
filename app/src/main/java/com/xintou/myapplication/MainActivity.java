3b62603258c4aa4a29f0a2b2168266399ab11232
popmj007 <892839806@qq.com>
2017-12-04 16:17:22 +0800

    添加textview

--- app/src/main/java/com/xintou/myapplication/MainActivity.java
+++ app/src/main/java/com/xintou/myapplication/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextView = new TextView(this);
+        mTextView = (TextView)findViewById(R.id.tv);
         mTextView.setText("荷兰老婆");
     }
 }
