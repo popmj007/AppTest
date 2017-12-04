7704a0e8b6c4c25795ec2aa980dafb3e029dd1d2
popmj007 <892839806@qq.com>
2017-12-04 15:04:12 +0800

    first commit

---
+++ app/src/main/java/com/xintou/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.xintou.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = new TextView(this);
+        mTextView.setText("荷兰老婆");
+    }
+}
